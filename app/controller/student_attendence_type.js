6880e05f4e0c592ac47ba145e196956dda0ff884
gesangseto <49031964+gesangseto@users.noreply.github.com>
2020-05-29 15:28:18 +0700

    Fix with console.log

--- app/controller/student_attendence_type.js
+++ app/controller/student_attendence_type.js
@@ -1,113 +1,26 @@
 'use strict';
-
 var response = require('../response');
 var connection = require('../connection');
-
-
 const perf = require('execution-time')();
 var dateFormat = require('dateformat');
 var datetime = require('node-datetime');
-
 var dt = datetime.create();
 var status_code = "";
 var messages = "";
 var elapseTime = "";
-exports.GetStudentAttendence = function (req, res) {
+exports.GetAttendenceType = function (req, res) {
     perf.start();
     console.log("date-time :" + new Date())
     console.log("api-name : " + req.originalUrl)
     console.log("body-sent : ")
     console.log(req.body)
-
-
-
-    var total = 0; connection.query("SELECT session_id FROM `sch_settings` ;",
-        function (error, result, fields) {
-            var session_id = result[0]['session_id']
-            var sql = `SELECT a.id as attendence_id,b.id as student_session_id, c.id as student_id, d.id as attendence_type_id,e.id as class_id, f.id as sub_class_id, d.type as type,a.*, concat(c.firstname,' ',c.lastname) AS name,
-            c.admission_no as admission_no, c.roll_no as roll_no,c.mobileno as mobileno , e.class as class,f.section as sub_kelas
-                            FROM student_attendences AS a
-                            JOIN student_session as b
-                            ON a.student_session_id = b.id
-                            JOIN students as c
-                            ON b.student_id = c.id
-                            JOIN attendence_type as d
-                            ON a.attendence_type_id=d.id
-                            JOIN classes as e on e.id=b.class_id
-                            JOIN sections as f on f.id=b.section_id
-                            WHERE  b.session_id=`+ session_id
-            if (req.query.class_id != undefined) {
-                sql = sql + ' AND b.class_id=' + req.query.class_id
-            }
-            if (req.query.sub_class_id != undefined) {
-                sql = sql + ' AND b.section_id=' + req.query.sub_class_id
-            }
-            if (req.query.date != undefined) {
-                sql = sql + ` AND a.date='` + req.query.date + `'`
-            }
-            if (req.query.student_id != undefined) {
-                sql = sql + ` AND b.student_id='` + req.query.student_id + `'`
-            }
-            sql = sql + `  ORDER BY b.student_id ASC`
-            connection.query(sql, function (error, result, fields) {
-                if (error) {
-                    messages = "Internal server error";
-                    elapseTime = perf.stop();
-                    elapseTime = elapseTime.time.toFixed(2);
-                    response.errorRes(elapseTime, messages, res);
-                } else {
-
-
-                    /*
-                    INI YANG MAU DI UBAH
-                    Test nya lewat postman
-                    */
-                    var rowsTranslate = [];
-                    var rowsTotal = []
-                    result.forEach(element => {
-                        // console.log(element, "date")
-                        // if (!rowsTranslate[element.student_id]) {
-                        //     rowsTranslate[element.student_id] = element
-
-                        //     rowsTranslate[element.student_id].attendence = [];
-                        //     rowsTranslate[element.student_id].attendence.push(element)
-                        // rowsTotal.push(element)
-                        // }
-                        total = total + 1;
-                        // console.log(rowsTranslate, "TRANSLETE")
-                    })
-                    /*
-                    BATAS AKHIR NYA DISINI
-                    */
-
-
-
-                    messages = "Success";
-                    elapseTime = perf.stop();
-                    elapseTime = elapseTime.time.toFixed(2);
-                    response.successGet(elapseTime, messages, total, result, res);
-                }
-            });
-        })
-
-
-};
-
-
-exports.GetStudentAttendenceById = function (req, res) {
-    perf.start();
-    console.log("date-time :" + new Date())
-    console.log("api-name : " + req.originalUrl)
-    console.log("body-sent : ")
-    console.log(req.body)
-
 
     var total = 0;
-    var sql = `SELECT * FROM student_attendences`
-    if (req.params.id != undefined) {
-        sql = sql + ' WHERE id=' + req.params.id
+    var sql = `SELECT * FROM attendence_type`
+    if (req.query.id != undefined) {
+        sql = sql + ' WHERE id=' + req.query.id
     }
-    sql = sql + ' ORDER BY id'
+    sql = sql + `  ORDER BY type ASC`
     connection.query(sql, function (error, result, fields) {
         if (error) {
             messages = "Internal server error";
@@ -118,22 +31,54 @@ exports.GetStudentAttendenceById = function (req, res) {
             result.forEach(element => {
                 total = total + 1;
             })
+
             messages = "Success";
             elapseTime = perf.stop();
             elapseTime = elapseTime.time.toFixed(2);
             response.successGet(elapseTime, messages, total, result, res);
         }
     });
+
+
 };
 
-
-exports.InsertStudentAttendence = function (req, res) {
+exports.GetAttendenceTypeById = function (req, res) {
     perf.start();
     console.log("date-time :" + new Date())
     console.log("api-name : " + req.originalUrl)
     console.log("body-sent : ")
     console.log(req.body)
 
+    var total = 0;
+    var sql = `SELECT * FROM attendence_type WHERE id=` + req.params.id
+    sql = sql + `  ORDER BY type ASC`
+    connection.query(sql, function (error, result, fields) {
+        if (error) {
+            messages = "Internal server error";
+            elapseTime = perf.stop();
+            elapseTime = elapseTime.time.toFixed(2);
+            response.errorRes(elapseTime, messages, res);
+        } else {
+            result.forEach(element => {
+                total = total + 1;
+            })
+
+            messages = "Success";
+            elapseTime = perf.stop();
+            elapseTime = elapseTime.time.toFixed(2);
+            response.successGet(elapseTime, messages, total, result, res);
+        }
+    });
+
+};
+
+
+exports.InsertAttendenceType = function (req, res) {
+    perf.start();
+    console.log("date-time :" + new Date())
+    console.log("api-name : " + req.originalUrl)
+    console.log("body-sent : ")
+    console.log(req.body)
 
     var total = 0;
     var body = req.body
@@ -145,8 +90,8 @@ exports.InsertStudentAttendence = function (req, res) {
     for (let key in body) {
         keys.push(key); // John, then 30
     }
-    var check_sql = `SELECT count(*) as count FROM student_attendences WHERE student_session_id=` + body.student_session_id + ` AND date='` + body.date + `'`
-    var sql = `INSERT INTO student_attendences (` + keys + `) VALUES (` + values + `)`;
+    var check_sql = `SELECT count(*) as count FROM attendence_type WHERE type='` + body.type + `'`
+    var sql = `INSERT INTO attendence_type (` + keys + `) VALUES (` + values + `)`;
     connection.query(check_sql,
         function (error, result, fields) {
             if (result[0]['count'] > 0) {
@@ -176,14 +121,13 @@ exports.InsertStudentAttendence = function (req, res) {
 
 
 
-exports.UpdateStudentAttendence = function (req, res) {
+exports.UpdateAttendenceType = function (req, res) {
     perf.start();
     console.log("date-time :" + new Date())
     console.log("api-name : " + req.originalUrl)
     console.log("body-sent : ")
     console.log(req.body)
 
-    var total = 0;
     var body = req.body
     var values = []
     var keys = []
@@ -202,8 +146,8 @@ exports.UpdateStudentAttendence = function (req, res) {
     myJSON = myJSON.replace(/["]/g, '');
     myJSON = myJSON.replace('[', '');
     myJSON = myJSON.replace(']', '');
-    var check_sql = `SELECT count(*) as count FROM student_attendences WHERE id!=` + body.id + ` AND student_session_id=` + body.student_session_id + ` AND date='` + body.date + `'`
-    var sql = `UPDATE student_attendences SET ` + myJSON + ` WHERE id=` + body.id;
+    var check_sql = `SELECT count(*) as count FROM attendence_type WHERE type='` + body.type + `' AND id!=` + body.id
+    var sql = `UPDATE attendence_type SET ` + myJSON + ` WHERE id = ` + body.id;
     connection.query(check_sql,
         function (error, result, fields) {
             if (result[0]['count'] > 0) {
@@ -228,6 +172,35 @@ exports.UpdateStudentAttendence = function (req, res) {
                     });
             }
         })
+
+
+};
+
+
+exports.DeleteAttendenceType = function (req, res) {
+    perf.start();
+    console.log("date-time :" + new Date())
+    console.log("api-name : " + req.originalUrl)
+    console.log("body-sent : ")
+    console.log(req.body)
+
+    var total = 0;
+    var id = req.params.id
+    var sql = `DELETE FROM attendence_type WHERE id = ` + id;
+    connection.query(sql,
+        function (error, result, fields) {
+            if (error) {
+                messages = "Internal server error";
+                elapseTime = perf.stop();
+                elapseTime = elapseTime.time.toFixed(2);
+                response.errorRes(elapseTime, messages, res);
+            } else {
+                messages = "Success Delete Data";
+                elapseTime = perf.stop();
+                elapseTime = elapseTime.time.toFixed(2);
+                response.successPost(elapseTime, messages, res);
+            }
+        });
 
 
 };
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function (app) {
-
     // Student Attendence
     var student_attendence = require('./controller/student_attendence');
     app.route('/attendence/student_attendence')
@@ -12,4 +11,18 @@ module.exports = function (app) {
         .put(student_attendence.InsertStudentAttendence);
     app.route('/attendence/student_attendence')
         .post(student_attendence.UpdateStudentAttendence);
+
+
+    // Tyep Attendence
+    var student_attendence_type = require('./controller/student_attendence_type');
+    app.route('/attendence/student_attendence_type')
+        .get(student_attendence_type.GetAttendenceType);
+    app.route('/attendence/student_attendence_type/:id')
+        .get(student_attendence_type.GetAttendenceTypeById);
+    app.route('/attendence/student_attendence_type')
+        .put(student_attendence_type.InsertAttendenceType);
+    app.route('/attendence/student_attendence_type')
+        .post(student_attendence_type.UpdateAttendenceType);
+    app.route('/attendence/student_attendence_type/:id')
+        .delete(student_attendence_type.DeleteAttendenceType);
 };
